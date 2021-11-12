4b70e0288316f264677ca5bcf470c31c1af0bbb2
flascardoso <flavia.cardoso@creditas.com>
2021-11-12 15:45:36 -0300

    First commit

---
+++ src/main/kotlin/com/br/lojinhadaflavia/LojinhaDaFlaviaApplication.kt
@@ -0,0 +1,11 @@
+package com.br.lojinhadaflavia
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LojinhaDaFlaviaApplication
+
+fun main(args: Array<String>) {
+	runApplication<LojinhaDaFlaviaApplication>(*args)
+}
---
+++ src/test/kotlin/com/br/lojinhadaflavia/LojinhaDaFlaviaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.br.lojinhadaflavia
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LojinhaDaFlaviaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
